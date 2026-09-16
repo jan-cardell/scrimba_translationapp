0524ad43d9ef298b01ae0c1388cd2ca02626a21c
jan-cardell <janpetercardell@outlook.de>
2026-09-16 10:54:09 +0200

    wire up ai api and backend

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const translateBtn = document.getElementById('translate-btn')
 const selectLanguage = document.getElementById('select-language')
+const outputDiv = document.getElementById('output-div')
 const outputText = document.getElementById('output-text')
 translateBtn.addEventListener('click', translate)
+const inputText = document.getElementById('ftext')
 
 
 async function translate(e){
     e.preventDefault()
-    const inputText = document.getElementById('ftext') 
     
     selectLanguage.style.display = 'none'
-    outputText.style.display = 'flex'
+    outputDiv.style.display = 'flex'
+    const inputTextValue = inputText.value
 
     try {
-        const response = await fetch('api/translation', {
+        const response = await fetch('http://localhost:3001/api/translation', {
         method: 'Post',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ inputText })    
+        body: JSON.stringify({ userPromt: inputTextValue })   
     })
 
-    const data = response.json()
+    const data = await response.json()
+    outputText.innerText = data.translation
+
     } catch(error) {
         console.log(error)
     }  
--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 import OpenAI from 'openai'
 import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const openai = new OpenAI({
-    apiKey: process.env.AI_KEY
+    apiKey: process.env.AI_KEY,
+    baseURL: process.env.AI_URL
 })
+
+const messages = [{
+    role: 'system',
+    content: 'You are a translator. Translate the user input into german.'
+}]
+
+app.post('/api/translation', async (req, res) => {
+    const { userPromt } = req.body
+
+    messages.push({
+        role: 'user',
+        content: userPromt
+    })
+
+    console.log(userPromt)
+
+    try {
+        const response = await openai.chat.completions.create({
+            model: process.env.AI_MODEL,
+            messages
+        })
+        const translation = response.choices[0].message.content
+
+        res.json({ translation })
+    } catch(e) {
+        console.log(e)
+    }
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
